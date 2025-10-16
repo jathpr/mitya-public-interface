5eea5e3c7c8e99dc45914d9c57d14f7ca2bbc67a
Dzmitry Martyniuk <jathpr@gmail.com>
2025-10-16 23:20:24 +0300

    remove static locale hook

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,9 +3,7 @@ import { Geist } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
-import { routing } from "@/i18n/routing";
 import LocaleSwitcher from "@/components/LocaleSwitcher";
-import { setRequestLocale } from "next-intl/server";
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -16,20 +14,11 @@ export const metadata: Metadata = {
   description: "See you soon",
 };
 
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
-}
-
 export default async function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
 }>) {
-  const { locale } = await params;
-  // Enable static rendering
-  setRequestLocale(locale);
   return (
     <html lang="en">
       <body className={`${geistSans.className}`}>
