9bcd89daefab4b4a101efcaa5a727e364b68101c
Dzmitry Martyniuk <jathpr@gmail.com>
2025-10-17 20:30:00 +0300

    add about translation, update locale to be

--- src/app/[lang]/(pages)/about/page.tsx
+++ src/app/[lang]/(pages)/about/page.tsx
@@ -1,19 +1,22 @@
-import { HyperLink } from "@/components/HyperLink";
 import styles from "./page.module.css";
 import mitya from "../../../../../public/me.jpg";
 import Image from "next/image";
 import Link from "next/link";
+import { getTranslations } from "next-intl/server";
+import { HyperLink } from "@/components/HyperLinkRich";
 
 export default async function about() {
+  const t = await getTranslations("about");
   return (
     <div className={styles.page}>
       <Link href="/buddhism/intro">
         <Image src={mitya} alt="mitya photo" className={styles.photo} />
       </Link>
       <p className={styles.text}>
-        Люблю даследваць розум па-за словамі. Гэтыя даследвання прывялі мяне да{" "}
-        <HyperLink url="/meditation" label="медытацыі" />. А медытацыя - да{" "}
-        <HyperLink url="/buddhism" label="Буддызму" />.
+        {t.rich("text", {
+          meditation: (c) => <HyperLink url="/meditation">{c}</HyperLink>,
+          buddhism: (c) => <HyperLink url="/buddhism">{c}</HyperLink>,
+        })}
       </p>
     </div>
   );
