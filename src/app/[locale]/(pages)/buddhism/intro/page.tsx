6186125ef2ffef348b8169ddc4e678ee79ab4414
Dzmitry Martyniuk <jathpr@gmail.com>
2025-10-19 19:01:18 +0300

    update video

--- src/app/[locale]/(pages)/buddhism/intro/page.tsx
+++ src/app/[locale]/(pages)/buddhism/intro/page.tsx
@@ -1,12 +1,31 @@
 import styles from "./page.module.css";
+import { Suspense } from "react";
+import { list } from "@vercel/blob";
 
 export default function video() {
   return (
-    <>
-      <video width="70%" controls className={styles.video}>
-        <source src={"./green.mp4"} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </>
+    <Suspense fallback={<p>Loading video...</p>}>
+      <VideoComponent fileName="green.mp4" />
+    </Suspense>
+  );
+}
+
+async function VideoComponent({ fileName }: { fileName: string }) {
+  const { blobs } = await list({
+    prefix: fileName,
+    limit: 1,
+  });
+  const { url } = blobs[0];
+
+  return (
+    <video
+      controls
+      aria-label="Video player"
+      width="70%"
+      className={styles.video}
+    >
+      <source src={url} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   );
 }
