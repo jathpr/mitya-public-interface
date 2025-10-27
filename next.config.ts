695f056137a0b3f3550ee453df67736ede72ab06
Dzmitry Martyniuk <jathpr@gmail.com>
2025-10-27 12:39:47 +0300

    nullified nextConfig

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  experimental: {
-    staleTimes: {
-      dynamic: 300,
-      static: 300,
-    },
-  },
+  // experimental: {
+  //   staleTimes: {
+  //     dynamic: 300,
+  //     static: 300,
+  //   },
+  // },
 };
 
 export default withNextIntl(nextConfig);
