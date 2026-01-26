fe8d86f1ce454d8d95c9f305795a9b78b5245366
Dzmitry Martyniuk <jathpr@gmail.com>
2026-01-27 00:07:09 +0300

    check lang

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({ children, params }: Props) {
   setRequestLocale(locale);
 
   return (
-    <html lang="en">
+    <html lang={locale}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
